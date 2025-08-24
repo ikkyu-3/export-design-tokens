58a0fa774930f27c306a9f1f12c76fdf87e17741
ikkyu-3 <toru.hasegawa.0521@gmail.com>
2025-08-24 21:50:53 +0900

    esbuildに変更

---
+++ build.mjs
@@ -0,0 +1,33 @@
+import * as esbuild from "esbuild";
+
+const isDev = !!process.argv.includes('--dev');
+const isWatch = !!process.argv.includes('--watch');
+
+const buildOptions = {
+    entryPoints: ['src/code.ts'],
+    outfile: 'dist/code.js',
+    bundle: true,
+    format: 'iife',
+    target: 'es6',
+    minify: !isDev,
+    sourcemap: isDev,
+    platform: "neutral",
+}
+
+if (isWatch) {
+    console.log('🔄 Watch mode')
+    const context = await esbuild.context(buildOptions)
+
+    console.log('👀 Watching for changes...')
+    await context.watch()
+} else {
+    console.log('🔨 Build mode')
+
+    try {
+        await esbuild.build(buildOptions)
+        console.log('✅  Build completed!')
+    } catch (error) {
+        console.error('❌ Build failed:', error)
+        process.exit(1)
+    }
+}
